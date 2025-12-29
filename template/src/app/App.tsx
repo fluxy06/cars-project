971924e70d047ad2e1713acec8e40b5aec6ac2c5
Никита Сорокин <nikitasorokin@MacBook-Air-Nikita-2.local>
2025-12-29 14:49:22 +0300

    added swipper module + declare/reform app.tsx

--- template/src/app/App.tsx
+++ template/src/app/App.tsx
@@ -1,61 +1,84 @@
-import '@app/App.css'
+import '@app/App.css';
 
 import React from 'react';
-import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
-import { Layout, Menu, theme } from 'antd';
-import HeaderMy from "layots/Header/Header"
-
-const { Header, Content, Footer, Sider } = Layout;
-
-const items = [UserOutlined, VideoCameraOutlined, UploadOutlined, UserOutlined].map(
-  (icon, index) => ({
-    key: String(index + 1),
-    icon: React.createElement(icon),
-    label: `nav ${index + 1}`,
-  }),
-);
+import HeaderMy from "layots/Header/Header";
+import Filters from '@features/Filters/Filters';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules'; // ← для стрелок
+import 'swiper/css';
+import 'swiper/css/navigation';
+import CarIO from '@assets/car/cars/cr1.svg?url';
+import CarIT from '@assets/car/cars/cr2.svg?url';
+import CarIH from '@assets/car/cars/cr3.svg?url';
+import CarIF from '@assets/car/cars/cr4.svg?url';
+import CardCar from '@features/CardCar/CardCar';
 
 const App: React.FC = () => {
-  const {
-    token: { colorBgContainer, borderRadiusLG },
-  } = theme.useToken();
-
   return (
-    <Layout>
-      <Sider
-        breakpoint="lg"
-        collapsedWidth="0"
-        onBreakpoint={(broken) => {
-          console.log(broken);
-        }}
-        onCollapse={(collapsed, type) => {
-          console.log(collapsed, type);
-        }}
-      >
-        <div className="demo-logo-vertical" />
-        <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={items} />
-      </Sider>
-      <Layout>
-        <Header style={{ padding: 0 , background: "transparent", height: "auto"}}>
-            <HeaderMy />
-        </Header> 
-        <Content style={{ margin: '24px 16px 0' }}>
-          <div
-            style={{
-              padding: 24,
-              minHeight: 360,
-              background: colorBgContainer,
-              borderRadius: borderRadiusLG,
+    <>
+      <div className='grid grid-cols-3 w-full h-auto'>
+        {/* Header */}
+        <div className='col-span-3 h-40'>
+          <HeaderMy logo='Nikita' />
+        </div>
+
+        {/* Filters */}
+        <div className='col-span-1 row-span-2 p-0'>
+          <Filters />
+        </div>
+
+        {/* Swiper container */}
+        <div className='col-span-2'>
+          <Swiper
+            spaceBetween={30}
+            slidesPerView={1}
+            loop={true} 
+            navigation={true}
+            modules={[Navigation]} 
+            breakpoints={{
+              640: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+              },
+              1024: {
+                slidesPerView: 3,
+                spaceBetween: 30,
+              },
             }}
+            className="mySwiper"
           >
-            content
-          </div>
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>
-          Ant Design ©{new Date().getFullYear()} Created by Ant UED
-        </Footer>
-      </Layout>
-    </Layout>
+            <SwiperSlide>
+              <CardCar
+                path={CarIF}
+                width={{ min: "200px", preferred: "23.75vw", max: "304px" }}
+                height={{ min: "260px", preferred: "30.3vw", max: "388px" }}
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <CardCar
+                path={CarIO}
+                width={{ min: "200px", preferred: "23.75vw", max: "304px" }}
+                height={{ min: "260px", preferred: "30.3vw", max: "388px" }}
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <CardCar
+                path={CarIT}
+                width={{ min: "200px", preferred: "23.75vw", max: "304px" }}
+                height={{ min: "260px", preferred: "30.3vw", max: "388px" }}
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <CardCar
+                path={CarIH}
+                width={{ min: "200px", preferred: "23.75vw", max: "304px" }}
+                height={{ min: "260px", preferred: "30.3vw", max: "388px" }}
+              />
+            </SwiperSlide>
+          </Swiper>
+        </div>
+      </div>
+    </>
   );
 };
 
---
+++ template/src/shared/Slider/Slider.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Slider } from 'antd';
+
+const Swap: React.FC = () => <Slider defaultValue={300000} tooltip={{ open: true }}/>;
+
+export default Swap;
